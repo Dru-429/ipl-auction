6c570c92207f0f1d611745b1f56a506a1d317a5d
Dru-429 <ect.dhruv@gmail.com>
2025-04-03 13:19:09 +0530

    Added Firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider} from 'firebase/auth'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKey = process.env.FIREBASE_API_KEY;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBXsL0xyqNQFL2GoLq6sGkxyWshrm5hvoQ",
+  apiKey: apiKey,
   authDomain: "auckit-ipl-auction.firebaseapp.com",
   projectId: "auckit-ipl-auction",
   storageBucket: "auckit-ipl-auction.firebasestorage.app",
@@ -18,4 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
